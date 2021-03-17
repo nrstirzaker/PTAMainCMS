278330f7a477c8654b3cd3222820df8deffbda81
Nigel <nrstirzaker.techblog@gmail.com>
2021-03-17 10:55:59 +0000

    updated members and meetings and email plugin

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({env}) => ({
+    // ...
+
+    email: {
+        provider: 'sendmail',
+        settings: {
+            defaultFrom: 'friendsatchristscollege@gmail.com',
+            defaultReplyTo: 'friendsatchristscollege@gmail.com',
+        },
+    }
+    // ...
+});
